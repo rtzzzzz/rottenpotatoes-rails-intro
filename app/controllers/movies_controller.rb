4f4d22177ede8c230d11eafa0fccc4a19ade2cd9
rtzzzzz <Zhang07@mail-alt.suddenlink.net>
2016-03-04 17:08:56 -0600

    hw 2 part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,29 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    redirect = false
+    if params[:ratings] != ratings_filter
+      redirect = true
+    end
+    if params[:sort] != sort_column
+      redirect = true
+    end
+
+    if session[:ratings] != ratings_filter
+      session[:ratings] = ratings_filter
+    end
+    if session[:sort] != sort_column
+      session[:sort] = sort_column
+    end
+
+    if redirect
+      flash.keep
+      redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
+    else
+      @all_ratings = Movie.ratings
+      @movies = Movie.find_by_ratings_and_order params[:ratings].keys, sort_column
+
+    end
   end
 
   def new
@@ -41,5 +63,24 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
+  private
 
+  def ratings_filter
+    if params[:ratings]
+       params[:ratings]
+    elsif !session[:ratings]
+       Hash[Movie.ratings.map { |x| [x, 1] }]
+    else
+       session[:ratings]
+    end
+  end
+
+  def sort_column
+     Movie.column_names.include?(params[:sort]) ? params[:sort] : session[:sort]
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  "asc" : "desc"
+  end
 end
