c06e41dd55201a01db1fb427a6274be54ec355bc
rtzzzzz <Zhang07@mail-alt.suddenlink.net>
2016-03-04 17:16:11 -0600

    hw 2 part 1 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+def sortable(column, title=nil, opts={})
+    title ||= column.titleize
+    class_name = sort_column == column ? 'hilite' : ''&& sort_direction == "asc" ? "desc" : "asc"
+    direction = sort_column == column && sort_direction == "asc" ? "desc" : "asc"
+    content_tag(:th, { :class => class_name }) do
+     	link_to title, params.merge({ :sort => column}), opts
+    end
+  end
 end
